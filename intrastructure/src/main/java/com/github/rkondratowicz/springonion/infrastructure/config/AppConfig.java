1678e91977272238822b48cc1423a5a237cb9ebe
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2024-02-22 21:59:24 +0100

    Add init classes for SpringBoot

---
+++ intrastructure/src/main/java/com/github/rkondratowicz/springonion/infrastructure/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.github.rkondratowicz.springonion.infrastructure.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class AppConfig {
+}
